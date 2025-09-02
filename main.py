0e20e6b7de95d6102e3b5c14675b72ef9a053ca3
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-03 07:17:22 +1100

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(layout="wide")
+pages_dict = {
+  "HOME"   : [st.Page("pages/Home.py", title="Home", icon=":material/elderly_woman:")],
+  "IMPORT" : [st.Page("pages/Import.py", title="Import files", icon=":material/upload:")],
+}
 
+pg = st.navigation( pages_dict, position="top" )
