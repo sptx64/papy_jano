82da1f3f701ec7126e7eefc9fd90845731951641
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-03 06:59:15 +1100

    Create Home.py

---
+++ pages/Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+"""
+# :material/elderly_woman: Papy Jano app 
+
+A small app to explore the streamlit dashboard capability for mine ops and mine planning operations.
+This app can :
+- 
+- 
+- 
+- 
+
+"""
