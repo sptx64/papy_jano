8dc49fd4e39562fd881abe4c0984b6fb227ad0a1
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-03 07:26:22 +1100

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-"""
-# :material/elderly_woman: Papy Jano app 
 
+"# :material/elderly_woman: Papy Jano app"
+"""
 A small app to explore the streamlit dashboard capability for mine ops and mine planning operations.
 This app can :
 - 
