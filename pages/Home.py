39ddb261de0402d16c60697054bfa2da0fb131ec
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-03 07:37:42 +1100

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -4,14 +4,5 @@ import numpy as np
 
 st.write("# :material/elderly_woman: Papy Jano app")
 
-st.write(
-"""
-A small app to explore the streamlit dashboard capability for mine ops and mine planning operations.
-This app can :
-- 
-- 
-- 
-- 
-
-"""
+st.write("A small app to explore the streamlit dashboard capability for mine ops and mine planning operations."
 )
