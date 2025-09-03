e80f8b62cf86d409e5de258fce82fd09b47993a8
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-03 17:53:14 +1100

    Update List_manager.py

--- pages/List_manager.py
+++ pages/List_manager.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+import os
 
 "# List manager"
 "On this page you can manage all options that you want to be added or removed to lists."
