3b0d2e26aa6e47c57611a01e78f1171acc8885e8
belsimon334-cmyk <bel.simon334@gmail.com>
2025-09-03 07:34:49 +0200

    Update Home.py

--- pages/Home.py
+++ pages/Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("# :material/elderly_woman: Papy Jano app")
+st.write("# :material/elderly_man: Papy Jano app")
 
 st.write("A small app to explore the streamlit dashboard capability for mine ops and mine planning operations."
 )
