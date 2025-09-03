58b31080d59cbf6f3bb8132b77b44e59d64a015a
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-03 16:46:42 +1100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ st.set_page_config(layout="wide")
 pages_dict = {
   "HOME"   : [st.Page("pages/Home.py", title="Home", icon=":material/elderly_woman:")],
   "IMPORT" : [st.Page("pages/Import.py", title="Import files", icon=":material/upload:")],
+  "APP DE BOLOSS" : [
+    st.Page("pages/app.py", title="vieille appli dégueux", icon=":material/delete:"),
+    st.Page("pages/visualization.py", title="visualisations pas ouf", icon=":material/earthquake:"),
+  ]
 }
 
 pg = st.navigation( pages_dict, position="top" )
