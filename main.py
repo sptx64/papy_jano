49728a26f5d58509b4e4c0bcde65aa741720e3f6
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-03 17:52:06 +1100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import numpy as np
 st.set_page_config(layout="wide")
 pages_dict = {
   "HOME"   : [st.Page("pages/Home.py", title="Home", icon=":material/elderly:")],
-  "IMPORT" : [st.Page("pages/Import.py", title="Import files", icon=":material/upload:")],
+  "IMPORT" : [
+    st.Page("pages/Import.py", title="Import files", icon=":material/upload:"),
+    st.Page("pages/List_manager.py", title="List manager", icon=":material/event_list:),
+             ],
   "APP DE BOLOSS" : [
     st.Page("pages/app.py", title="vieille appli dégueux", icon=":material/delete:"),
     st.Page("pages/visualizations.py", title="visualisations pas ouf", icon=":material/earthquake:"),
