b0de1d75773d84e6dc0febe2e40219930cf6ad94
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-05 17:45:17 +1100

    Update log.py

--- func/log.py
+++ func/log.py
@@ -22,7 +22,7 @@ def log() :
     
       projects = [ n for n in os.listdir(ffolder) if os.path.isdir(os.path.join(ffolder,n)) ]
       
-      c = st.colums(2)
+      c = st.columns(2)
       disabled=True
       new_project = c[1].toggle("Create a New project", value=True)
       if new_project :
