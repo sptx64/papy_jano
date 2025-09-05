680a1017a2680256e2d6380d56d4eec246017dc3
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-05 17:33:29 +1100

    Update log.py

--- func/log.py
+++ func/log.py
@@ -13,32 +13,37 @@ def log() :
 
 
   if not "project" in st.session_state :
-    ffolder = get_save_folder()
     
-    projects = [ n for n in os.listdir(ffolder) if os.path.isdir(os.path.join(ffolder,n)) ]
-    "## Log to your project"
-    c = st.colums(2)
-    disabled=True
-    new_project = c[1].toggle("Create a New project", value=True)
-    if new_project :
-      project=c[0].text_input("New project name", None, placeholder="YOUR NEW PROJECT NAME")
-      if project in projects or project is None :
-        st.warning("This project name is empty or already exists")
-      else :
-        disabled=False
-
-    else :
-      project=st.selectbox("Select your existing project", projects, None)
-      if project is not None :
-        disabled=False
-       
-    if st.button("Log in", type="primary", disabled=disabled) :
-      filepath = os.path.join(ffolder,project)
+    @st.dialog("Log in", dismissible=False)
+    def log_modal() :
+      ffolder = get_save_folder()
+    
+      projects = [ n for n in os.listdir(ffolder) if os.path.isdir(os.path.join(ffolder,n)) ]
+      
+      c = st.colums(2)
+      disabled=True
+      new_project = c[1].toggle("Create a New project", value=True)
       if new_project :
-        os.mkdir(filepath)
+        project=c[0].text_input("New project name", None, placeholder="YOUR NEW PROJECT NAME")
+        if project in projects or project is None :
+          st.warning("This project name is empty or already exists")
+        else :
+          disabled=False
+
+      else :
+        project=st.selectbox("Select your existing project", projects, None)
+        if project is not None :
+          disabled=False
+       
+      if st.button("Log in", type="primary", disabled=disabled) :
+        filepath = os.path.join(ffolder,project)
+        if new_project :
+          os.mkdir(filepath)
       
-      st.session_state.project=filepath
-      st.rerun()
-      
+        st.session_state.project=filepath
+        st.rerun()
+      else :
+        st.stop()
+        
 
     
