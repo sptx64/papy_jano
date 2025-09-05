3a4e1b2ecc481a3f90df43fcf3724d429da6ff55
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-06 09:54:55 +1100

    Update all_class.py

--- func/all_class.py
+++ func/all_class.py
@@ -7,23 +7,33 @@ this file store all classes available
 """
 
 class MachineEntity :
-  def __init__(self, mtype, id, name, capacity, comment, availability) :
-    self.mtype = machine_type
-    self.id = machine_id
-    self.name = machine_name
-    self.capacity = machine_capacity
-    self.comment = machine_comment
+  def __init__(self, name, mtype, id, name, capacity, comment, availability) :
+    """
+    This function initiate the MachineEntity class called in MineOps
+    """
+    self.name         = name
+    self.mtype        = machine_type
+    self.id           = machine_id
+    self.name         = machine_name
+    self.capacity     = machine_capacity
+    self.comment      = machine_comment
     self.availability = machine_availability
 
 
 class MineOps :
-  def __init__(self, dict_opt, mine_supervisors={}, mine_fleet={}, mine_task={}) :
-    self.dict_opt = dict_opt
+  def __init__(self, dict_opt={}, mine_supervisors={}, mine_fleet={}, mine_task={}) :
+    """
+    This function initiate the MineOps class
+    """
+    self.dict_opt         = dict_opt
     self.mine_supervisors = mine_supervisors
-    self.mine_fleet = mine_fleet
-    self.mine_task = mine_task
+    self.mine_fleet       = mine_fleet
+    self.mine_task        = mine_task
 
   def check_dict_opt(self, to_check) :
+    """
+    This function checks if dict_opt is empty then if mine_supervisors, mine_fleet or mine_task sub classes are empty 
+    """
     if len(self.dict_opt) == 0 :
       st.error("Class MineOps dict_opt is empty, please create one in the List manager page.")
       st.stop()
@@ -36,10 +46,13 @@ class MineOps :
         else :
           st.error(f"The key {k} does not exist, please create it in the List manager page.")
       
-  def fleet_setup(self) :
+  def create_fleet(self) :
+    """
+    This function create mine fleet in the MineOps class
+    """
     check_dict_opt("mine_fleet")
     machine_type = self.dict_opt["Machines"]
-    fleet={}
+    fleet = {}
     t = st.tabs(machine_types)
     for i,mt in enumerate(machine_type) :
       with t[i] :
@@ -84,6 +97,39 @@ class MineOps :
 
         else :
           st.info("Please select/enter a valid name")
+
+  def create_dict_opt(self) :
+    mops_name = st.text_input("New MineOps name", None, placeholder="YOUR NEW MineOps name")
+    if mops_name is None or mops_name == "" :
+      st.info("Enter a name for your new MineOps")
+      st.stop()
+    mops_prefix = "MineOps - "
+    list_existing_mops = [ x.replace(mops_prefix,"").replace(".pkl","") for x in os.listdir(st.session_state.project) if x.startswith(mops_prefix) and x.endswith(".pkl") ]
+    if mops_name in list_existing_mops :
+      st.warning("This MineOps name already exists.")
+      st.stop()
+    list_task = st.multiselect(":material/assignment_add: Task options that will be available in other modules", [], [], accept_new_options=True)
+    list_supervisors = st.multiselect(":material/man: Supervisors options that will be available in other modules", [], [], accept_new_options=True)
+    list_machines = st.multiselect(":material/conveyor_belt: Type of machines that will be available in other modules", [], [], accept_new_options=True)
+    
+    #one unique save button that will save all lists in a dictionary and deleting any duplicate option
+    if st.button("Save", type="primary", help="Save your new MineOps") :
+      dict_opt={
+        "Task" : sorted(list(np.unique(list_task)), key=str.lower),
+        "Supervisors" : sorted(list(np.unique(list_supervisors)), key=str.lower),
+        "Machines" : sorted(list(np.unique(list_machines)), key=str.lower),
+      }
+      self.dict_opt=dict_opt
+
+      
+      # #storing it locally for next sessions
+      # full_name = os.path.join(folder, f"{fname_begin}.pkl")
+      # with open(full_name, "wb") as f :
+      #   pickle.dump(dict_opt, f)
+      #   st.toast("The options have been saved!", icon=":material/check_small:")
+      #   st.success("Saved! The app will rerun in 3 seconds.")
+      #   time.sleep(3)
+      #   st.rerun()
             
             
             
