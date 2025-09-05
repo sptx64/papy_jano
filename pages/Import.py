c453f11f0c889e5f639c92a49ab32dcac034faac
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-05 17:52:40 +1100

    Update Import.py

--- pages/Import.py
+++ pages/Import.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+from func.log import log
+log()
+
+
 "# :material/upload: Import data"
 
 list_type=["Work shift","Pit","Topography","Block Model"]
