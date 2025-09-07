0733ba25081ccd90f78a6147dfd43d7a8d0e9006
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-08 07:40:55 +1100

    Update all_class.py

--- func/all_class.py
+++ func/all_class.py
@@ -129,7 +129,7 @@ class MineOps :
         res.append(arr)
 
     df = pd.DataFrame(res, columns=col_name)
-    st.data_editor(df)
+    st.data_editor(df, height=750, hide_index=True, disabled=["Machine type", "Machine ID"])
       
 
   
