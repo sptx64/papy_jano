3bb2df5b161bb2ef9c27e2d3d14e211b61697174
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-07 17:55:33 +1100

    Update all_class.py

--- func/all_class.py
+++ func/all_class.py
@@ -81,19 +81,19 @@ class MineOps :
       dict_machine_entities = {}
         
       for mt_id in range(nb_mt) :
-        c = st.columns(4)
+        c = st.columns(4, vertical_alignment="bottom")
         machine_name         = c[0].text_input(f"{mt} - id:{mt_id} name", f"{mt}-{mt_id}")
         machine_capacity     = c[1].number_input(f"{mt} - id:{mt_id} capacity (tons)", capacity_range[0], capacity_range[1], default_capacity, help="Can be left empty")
         machine_comment      = c[2].text_input(f"{mt} - id:{mt_id} comment", None, placeholder="Any comment you want to link to this machine", help="Can be left empty if no comment")
-        machine_availability = c[3].checkbox(f"{mt} - id:{mt_id} is available", value=True)
+        machine_availability = c[3].toggle(f"{mt} - id:{mt_id} is available", value=True)
         machine_entity       = MachineEntity(mtype=mt, id=mt_id, name=machine_name,
                                          capacity=machine_capacity, comment=machine_comment,
                                          availability=machine_availability)
           
         dict_machine_entities[mt_id] = machine_entity
-      ""
-      "---"
-      ""
+      st.write("")
+      st.write("---")
+      st.write("")
       fleet[mt] = dict_machine_entities
     
     if st.button("Save your MineFleet in your MineOps", type="primary") :
