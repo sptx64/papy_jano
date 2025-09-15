1986ff29cfb7f1812897d406e94ea1b3efd041d8
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-09-15 16:54:17 +1100

    Update Task_scheduler.py

--- pages/Task_scheduler.py
+++ pages/Task_scheduler.py
@@ -62,23 +62,19 @@ df_grp = df[["category","count"]].groupby("category").sum()
 # rajouter pie echarts
 # rajouter la suite
 
-
+res = [ {"value" : v, "name":n } for n,v in zip(df_grp["category"].values, df_grp["count"].values) ]
 option = {
   "title" : {"text" : 'Referer of a Website', "subtext" : 'Fake Data', "left" : 'center'},
   "tooltip" : { "trigger" : 'item'},
   "legend"  : {"orient" : 'vertical', "left" : 'left'},
   "series"  : [
         { "name" : 'Access From', "type"   : 'pie', "radius" : '50%',
-          "data" : [
-            { "value" : 1048, "name" : 'Search Engine' },
-            { "value" : 735,  "name" : 'Direct' },
-            { "value" : 580,  "name" : 'Email' },
-            { "value" : 484,  "name" : 'Union Ads' },
-            { "value" : 300,  "name" : 'Video Ads' }
-          ],
+          "data" : res,
           "emphasis" : {
             "itemStyle" : { "shadowBlur" : 10, "shadowOffsetX" : 0, "shadowColor" : 'rgba(0, 0, 0, 0.5)' }
           }
         }
       ]
 }
+
+st_echarts(options=options, height="600px",)
