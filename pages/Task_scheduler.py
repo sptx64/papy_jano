41257f080288ebc00f3135223d3455f69c655aa9
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-10-09 21:07:29 +1100

    Update Task_scheduler.py

--- pages/Task_scheduler.py
+++ pages/Task_scheduler.py
@@ -129,8 +129,8 @@ with c[1] :
 mine_fleet = minops.mine_fleet
 mine_fleet
 mine_task
-mine_fleet["Truck"][0]
-{ k:len([x for x in mine_fleet[k] if mine_fleet[k][int(x)].machine_availability == True]) for k in mine_fleet },
+mine_fleet["Truck"][0].availability
+{ k:len([x for x in mine_fleet[k] if mine_fleet[k][int(x)].availability == True]) for k in mine_fleet },
 
 if st.button("Go") :
   mine_fleet = minops.mine_fleet
