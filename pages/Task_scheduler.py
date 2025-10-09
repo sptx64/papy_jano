259ea271aaf236e39c70322a3ea9e970e9592b40
sptx64 <89103497+sptx64@users.noreply.github.com>
2025-10-09 21:04:15 +1100

    Update Task_scheduler.py

--- pages/Task_scheduler.py
+++ pages/Task_scheduler.py
@@ -129,7 +129,8 @@ with c[1] :
 mine_fleet = minops.mine_fleet
 mine_fleet
 mine_task
-mine_fleet["Truck"]
+mine_fleet["Truck"][0]
+mine_fleet["Truck"]["0"]
 { k:len([x for x in mine_fleet[k] if mine_fleet[k][x].machine_availability == True]) for k in mine_fleet },
 
 if st.button("Go") :
